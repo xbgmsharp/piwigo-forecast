b78d74e006a0cb8067f74791c37761d310ba7014
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-09 06:13:27 +0200

    Add es_ES, thanks to jpr928

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['LANGUAGE_DESC'] = 'Seleccione el idioma deseado. Tenga en cuenta que las unidades en el resumen se establecerán de acuerdo con la opción de unidades, arriba, así que asegúrese de configurar las dos opciones, según sea necesario.';
+$lang['PLG_FORECAST'] = 'Este plugin muestra información local y fecha a partir de una imagen o vídeo.';
+$lang['SHOWLOCATION'] = 'Mostrar mapa local';
+$lang['SHOWLOCATION_DESC'] = 'Mostrar un mapa a la derecha del panel de la pagina foto.';
+$lang['SHOW_LINK'] = 'Mostrar "Visto en previsión"';
+$lang['SHOW_LINK_DESC'] = 'Añadir un enlace después para visualizarlo en Forecast.io.';
+$lang['UNIT'] = 'Unidad';
+$lang['UNIT_DESC'] = 'Seleccióna la unidad adecuada. "Auto" por defecto';
+$lang['VIEW_FORECAST'] = 'Visto en "Forecast.io';
+$lang['ADD_BEFORE'] = 'Añadir ubicación antes';
+$lang['ADD_BEFORE_DESC'] = 'Donde mostrar la información de ubicación.';
+$lang['API_KEY'] = 'Clave API';
+$lang['API_KEY_DESC'] = 'Opcional, ingrese su clave de API Forecast.io otra manera, es utilizar el proxy.';
+$lang['COLOR_BKG'] = 'Color de fondo';
+$lang['COLOR_BKG_DESC'] = 'Color de fondo, blanco por defecto';
+$lang['COLOR_TEXT'] = 'Color del texto';
+$lang['COLOR_TEXT_DESC'] = 'Color del texto e icono, negro por defecto';
+$lang['DISPLAY'] = 'Mostrar la configuración';
+$lang['HEADER'] = 'Nombre del encabezado';
+$lang['HEADER_DESC'] = 'Escriba el nombre para mostrar en la parte superior el detalle escondido.';
+$lang['LANGUAGE'] = 'Idioma';
