91b005a6a71c8475c9012cf68f3666d633aa4dc1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-09 06:13:39 +0200

    Add ru_RU, thanks to Konve

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['VIEW_FORECAST'] = 'Просмотр на Forecast.io';
+$lang['SHOW_LINK_DESC'] = 'Добавить ссылку для просмотра на Forecast.io.';
+$lang['HEADER_DESC'] = 'Введите имя для отображения сверху.';
+$lang['UNIT'] = 'Единицы измерения';
+$lang['UNIT_DESC'] = 'Выберите подходящие единицы измерения. По умолчанию выбор автоматический.';
+$lang['SHOW_LINK'] = 'Отображать "Просмотр погоды"';
+$lang['SHOWLOCATION_DESC'] = 'Показать карту в правой панели на странице изображения';
+$lang['SHOWLOCATION'] = 'Отображение карты';
+$lang['LANGUAGE_DESC'] = 'Выберите нужный язык. Еще учтите, что будут использоваться единицы измерения, выбранные ранее. Так что установите оба параметра как Вам необходимо.';
+$lang['COLOR_TEXT_DESC'] = 'Цвет текста и анимированных значков по умолчанию - черный.';
+$lang['COLOR_BKG_DESC'] = 'По умолчанию цвет фона - белый.';
+$lang['API_KEY'] = 'API KEY';
+$lang['ADD_BEFORE'] = 'Сначала добавить местоположение';
+$lang['ADD_BEFORE_DESC'] = 'Где отображать информацию о местоположении.';
+$lang['COLOR_BKG'] = 'Цвет фона';
+$lang['API_KEY_DESC'] = 'Дополнительно, введите Ваш Forecast.io API KEY, в противном случае используется прокси.';
+$lang['DISPLAY'] = 'Настройка отображения';
+$lang['HEADER'] = 'Имя заголовка';
+$lang['COLOR_TEXT'] = 'Цвет текста';
+$lang['LANGUAGE'] = 'Язык';
+$lang['PLG_FORECAST'] = 'Этот плагин показывает состояние погоды в то время и в том месте, где было сделано изображение или видео';
