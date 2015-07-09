ccbf293411db0d0563ca1818b3b470e736c01b13
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-09 06:13:06 +0200

    Update da_DK, thanks to Kaare

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -20,4 +20,24 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['ADD_BEFORE'] = 'test';
+$lang['ADD_BEFORE'] = 'Tilføj vejr før';
+$lang['VIEW_FORECAST'] = 'Se hos Forecast.io';
+$lang['DISPLAY'] = 'Visningsopsætning';
+$lang['HEADER'] = 'Headers navn';
+$lang['HEADER_DESC'] = 'Angiv navn der skal vises oven på overskyet-detaljer.';
+$lang['LANGUAGE'] = 'Sprog';
+$lang['LANGUAGE_DESC'] = 'Vælg det ønskede sprog. Vær opmærksom på, at enheder i resumet vil blive opsat jævnfør enhedsopsætningen oven for, så sørg for at opsætte begge indstillinger som ønsket.';
+$lang['PLG_FORECAST'] = 'Plugin\'en viser vejrudsigtsforhold ud fra et billedes eller videos placering og dato.';
+$lang['SHOWLOCATION'] = 'Vis placeringskort';
+$lang['SHOWLOCATION_DESC'] = 'Vis et kort i det højre panel på billedsiden.';
+$lang['SHOW_LINK'] = 'Vis "Se hos Forecast".';
+$lang['SHOW_LINK_DESC'] = 'Tilføj et link efter gråvejret for at se det hos Forecast.io.';
+$lang['UNIT'] = 'Enhed';
+$lang['UNIT_DESC'] = 'Vælg de relevante enheder. Standard er auto.';
+$lang['ADD_BEFORE_DESC'] = 'Hvor vejroplysninger skal vises.';
+$lang['API_KEY'] = 'API-nøgle';
+$lang['API_KEY_DESC'] = 'Valgfri, angiv din API-nøgle til Forecast.io eller brug proxy\'en.';
+$lang['COLOR_BKG'] = 'Farve, baggrund';
+$lang['COLOR_BKG_DESC'] = 'Baggrundsfarve, standard er hvid.';
+$lang['COLOR_TEXT'] = 'Farve, tekst';
+$lang['COLOR_TEXT_DESC'] = 'Farve på tekst og animeringsikon, standard er sort.';
