d89afaa1c6077d8aed153f2b089217716437c9b6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-14 08:17:55 +0200

    Add nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ADD_BEFORE'] = 'Geef weer-informatie weer voor';
+$lang['ADD_BEFORE_DESC'] = 'Waar dient de weer-informatie weergegeven te worden?';
+$lang['API_KEY'] = 'API sleutel';
+$lang['API_KEY_DESC'] = 'Optioneel, voer uw Forecast.io API sleutel in, of gebruik de procy.';
+$lang['COLOR_BKG'] = 'Kleur achtergrond';
+$lang['COLOR_BKG_DESC'] = 'Kleur van de achtergrond, standaard is wit.';
+$lang['COLOR_TEXT'] = 'Kleur tekst';
+$lang['COLOR_TEXT_DESC'] = 'Kleur van de tekst en animatie-ikoon, standaard is zwart.';
+$lang['HEADER'] = 'Tekst van de kopregel';
+$lang['HEADER_DESC'] = 'Tekst die boven de voorspelling weergegeven dient te worden.';
+$lang['LANGUAGE'] = 'Taal';
+$lang['LANGUAGE_DESC'] = 'Selecteer de gewenste taal. NB Eenheden in de samenvatting worden weergegeven op basis van de hierboven ingestelde Eenheid-optie. Stel dus beide opties in, indien noodzakelijk.';
+$lang['PLG_FORECAST'] = 'Deze plugin geeft weer-informatie aan de hand van de locatie en datum van een afbeelding of video.';
+$lang['SHOW_LINK'] = 'Geef weer \'Bekijk bij weer-informatie\'';
+$lang['SHOW_LINK_DESC'] = 'Voeg een link naar Forecast.io toe na de weer-informatie.';
+$lang['UNIT'] = 'Eenheid';
+$lang['UNIT_DESC'] = 'Selecteer de relevante eenheid. Standaard is auto.';
+$lang['VIEW_FORECAST'] = 'Bekijk op Forecast.io';
