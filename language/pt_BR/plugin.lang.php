a4f038fcce8d3cea503d560c0ee219b905fb1d92
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-14 08:18:02 +0200

    Add pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ADD_BEFORE'] = 'Coloque o tempo antes';
+$lang['ADD_BEFORE_DESC'] = 'Onde apresentar a informação meteorológica.';
+$lang['API_KEY'] = 'API KEY';
+$lang['API_KEY_DESC'] = 'Opcional, insira a sua CHAVE API do Forecast.io de outra forma usar o proxy.';
+$lang['COLOR_BKG'] = 'Cor de Fundo';
+$lang['COLOR_BKG_DESC'] = 'Cor de fundo, o padrão é branco.';
+$lang['COLOR_TEXT'] = 'Cor do texto';
+$lang['COLOR_TEXT_DESC'] = 'Cor do texto e do ícone animado, o padrão é preto.';
+$lang['HEADER'] = 'Nome do cabeçalho';
+$lang['HEADER_DESC'] = 'Digite o nome a ser exibido na parte superior do detalhe escuro.';
+$lang['LANGUAGE'] = 'Idioma';
+$lang['LANGUAGE_DESC'] = 'Selecione o idioma desejado. Informamos que unidades no resumo será definido de acordo com a opção de unidades, acima, portanto, certifique-se de definir ambas as opções, conforme necessário.';
+$lang['PLG_FORECAST'] = 'Este plugin exibe a previsão do tempo a partir do local e da data de uma imagem ou vídeo.';
+$lang['SHOW_LINK'] = 'Exibir \'Ver em Previsão do Tempo\'';
+$lang['SHOW_LINK_DESC'] = 'Adicionar um link após nublado para vê-lo em Forecast.io.';
+$lang['UNIT'] = 'Unidade';
+$lang['UNIT_DESC'] = 'Selecione as unidades relevantes. O padrão é auto.';
+$lang['VIEW_FORECAST'] = 'Visualizar em Forecast.io';
