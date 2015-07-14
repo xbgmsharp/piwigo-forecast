7d6dfbfe5aebba0f05ba9ed105260e5357ea6a19
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-14 08:17:51 +0200

    Add en_US, thanks to thunderrabbit

---
+++ language/en_US/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['API_KEY_DESC'] = 'Optional, input your Forecast.io API KEY, or use the proxy.';
+$lang['COLOR_BKG_DESC'] = 'Background color (default white)';
+$lang['COLOR_TEXT'] = 'Text color';
+$lang['COLOR_TEXT_DESC'] = 'Text and icon animation color (default black)';
+$lang['LANGUAGE_DESC'] = 'Select the desired language. Please note that units in the summary will be set according to the units option above, so be sure to set both options as needed.';
+$lang['PLG_FORECAST'] = 'This plugin displays forecast conditions based on the date and location of an image or video.';
