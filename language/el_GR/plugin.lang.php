224427511b5754ea1999128cf47ebd1b95739ffd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-19 10:50:12 +0200

    Add el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UNIT'] = 'Μονάδες';
+$lang['VIEW_FORECAST'] = 'Δείτε το στο Forecast.io';
+$lang['UNIT_DESC'] = 'Επιλέξτε τις σχετικές μονάδες. Η προεπιλογή είναι auto.';
+$lang['SHOW_MODE_DESC'] = 'Επιλέξτε τη γραφικη ή λειτουργία κειμένου για το widget';
+$lang['SHOW_MODE'] = 'Εμφάνιση σε λειτουργία γραφικών ή κειμένου ';
+$lang['SHOW_LINK_DESC'] = 'Προσθέστε ένα σύνδεσμο μετά τη λεζάντα για να τη δείτε στο Forecast.io.';
+$lang['SHOW_LINK'] = 'Εμφάνιση «Δείτε στις προβλέψεις»';
+$lang['PLG_FORECAST'] = 'Αυτό το πρόσθετο εμφανίζει πρόβλεψη από την τοποθεσία και την ημερομηνία μιας εικόνας ή βίντεο.';
+$lang['LANGUAGE_DESC'] = 'Επιλέξτε την επιθυμητή γλώσσα. Σας ενημερώνουμε ότι οι μονάδες στην περίληψη θα ρυθμιστούν σύμφωνα με την επιλογή μονάδων, πάνω, σιγουρευτείτε΄ότι ρυθμίσατε και τις δύο επιλογές, όπως απαιτείται.';
+$lang['LANGUAGE'] = 'Γλώσσα';
+$lang['HEADER_DESC'] = 'Πληκτρολογήστε το όνομα που θα εμφανίζεται στο πάνω μέρος της λεπτομέρειας της λεζάντας.';
+$lang['HEADER'] = 'Όνομα της επικεφαλίδας';
+$lang['COLOR_TEXT_DESC'] = 'Χρώμα του εικονιδίου κειμένου και κινούμενου icon, η προεπιλογή είναι μαύρο.';
+$lang['COLOR_TEXT'] = 'Χρώμα Κειμένου';
+$lang['COLOR_BKG_DESC'] = 'Χρώμα του φόντου, προεπιλογή είναι το λευκό.';
+$lang['COLOR_BKG'] = 'Χρώμα φόντου';
+$lang['API_KEY_DESC'] = 'Προαιρετικά, εισάγετε το Forecast.io API κλειδί σας διαφορετικά χρησιμοποιήσετε proxy.';
+$lang['API_KEY'] = 'API KEY';
+$lang['ADD_BEFORE_DESC'] = 'Πού να εμφανίζονται οι πληροφορίες καιρού.';
+$lang['ADD_BEFORE'] = 'Προσθέστε τον Καιρό πριν';
