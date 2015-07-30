05e359f8b42b7b7ece5ffe7bd65fed2c7c2cec8a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-30 15:36:02 +0200

    Add uk_UA, thanks to m_alien

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['SHOW_LINK'] = 'Показати "Переглянути на Forecast"';
+$lang['LANGUAGE_DESC'] = 'Виберіть потрібну мову. Зверніть увагу, що одиниці вимірювання буде встановлено відповідно до опції одиниць вимірювання, вище, так що не забудьте встановити обидва варіанти в разі потреби.';
+$lang['PLG_FORECAST'] = 'Цей плагін відображує стан погоди з координат розташуваня та дати зображення чи відео.';
+$lang['HEADER_DESC'] = 'Введіть назву для відображення зверху над деталями погоди.';
+$lang['COLOR_TEXT_DESC'] = 'Колір тексту та анімації іконок, за замовчуванням чорний.';
+$lang['COLOR_BKG_DESC'] = 'Колір фону, за замовчуванням білий.';
+$lang['API_KEY_DESC'] = 'Опціонально, введіть ваш Forecast.io API KEY, інакше використовуйте проксі.';
+$lang['ADD_BEFORE'] = 'Спочатку додайте погоду';
+$lang['ADD_BEFORE_DESC'] = 'Де показувати інформацію про погоду.';
+$lang['API_KEY'] = 'API KEY';
+$lang['HEADER'] = 'Назва заголовку';
+$lang['SHOW_LINK_DESC'] = 'Додати посилання для перегляду на Forecast.io.';
+$lang['SHOW_MODE_DESC'] = 'Оберіть графічний чи текстовий режим для віджету';
+$lang['SHOW_MODE'] = 'Показувати у графічному чи текстовому режимі';
+$lang['VIEW_FORECAST'] = 'Переглянути на Forecast.io';
+$lang['UNIT_DESC'] = 'Оберіть відповідні одиниці вимірювання. За замовчанням автоматично.';
+$lang['UNIT'] = 'Одиниці вимірювання';
+$lang['COLOR_BKG'] = 'Колір фону';
+$lang['COLOR_TEXT'] = 'Колір тексту';
+$lang['LANGUAGE'] = 'Мова';
