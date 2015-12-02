f98bd39700973579cec5bdf5d570f3b744ccecf5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-12-02 05:49:36 +0100

    Add zh_CN, thanks to dennisyan

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PLG_FORECAST'] = '本插件根据照片或视频的拍摄地点和日期显示天气状况。';
+$lang['ADD_BEFORE'] = '在此之前添加天气：';
+$lang['SHOW_LINK'] = '显示\'在Forecast上查看\'';
+$lang['LANGUAGE_DESC'] = '选择语言。请注意，概要中的单位将按照上方的单位选项来设定，所以请按需要同时设定好两个选项。';
+$lang['LANGUAGE'] = '语言';
+$lang['SHOW_LINK_DESC'] = '在预报后面添加一个链接，以在Forecast.io上查看';
+$lang['SHOW_MODE'] = '以图形或文本模式显示';
+$lang['SHOW_MODE_DESC'] = '为工具选择图形/文本模式';
+$lang['UNIT'] = '单位';
+$lang['UNIT_DESC'] = '选择相关单位，默认为自动。';
+$lang['COLOR_BKG'] = '背景颜色';
+$lang['COLOR_BKG_DESC'] = '背景颜色，默认为白色。';
+$lang['COLOR_TEXT'] = '文本颜色';
+$lang['COLOR_TEXT_DESC'] = '文本颜色与动画图标，默认为黑色。';
+$lang['HEADER'] = '页眉名称';
+$lang['HEADER_DESC'] = '输入显示于预报详情顶部的名称。';
+$lang['API_KEY_DESC'] = '可选，输入您的API密钥，或使用代理。';
+$lang['ADD_BEFORE_DESC'] = '在何处显示天气信息。';
+$lang['API_KEY'] = 'API密钥';
+$lang['VIEW_FORECAST'] = '在Forecast.io上查看';
