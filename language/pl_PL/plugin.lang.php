0eff1f87ec71cfddc2551c456fee2c416dfffaef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-01-08 06:55:49 +0100

    Add pl_PL, thanks to kuba

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['COLOR_BKG_DESC'] = 'Kolor tła, domyślnie biały';
+$lang['COLOR_TEXT_DESC'] = 'Kolor tekstu i animowanej ikony, domyślnie czarny';
+$lang['HEADER'] = 'Nazwa nagłówka';
+$lang['PLG_FORECAST'] = 'Ta wtyczka wyświetla informację o warunkach pogodowych z lokalizacji i daty zdjęcia lub filmu';
+$lang['UNIT_DESC'] = 'Wybierz odpowiednią jednostkę. Domyślnie auto';
+$lang['VIEW_FORECAST'] = 'Zobacz na Forecast.io';
+$lang['ADD_BEFORE_DESC'] = 'Gdzie wyświetlać informacje o pogodzie';
+$lang['API_KEY'] = 'KLUCZ API';
+$lang['COLOR_BKG'] = 'Kolor tła';
+$lang['COLOR_TEXT'] = 'Kolor tekstu';
+$lang['LANGUAGE'] = 'Język';
+$lang['UNIT'] = 'Jednostka';
