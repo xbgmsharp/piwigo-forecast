2596cee015471bad9d1f666e255127f38186c88f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-12-04 13:33:33 +0100

    Add lv_LV, thanks to agrisans

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['LANGUAGE_DESC'] = '
+Izvēlieties vēlamo valodu. Lūdzu, ņemiet vērā, ka vienības kopsavilkumā tiks noteiktas atkarībā no vienības izvēlnē noteiktās, tāpēc pārliecinieties, lai uzstādītu abas opcijas, kā nepieciešams.';
+$lang['PLG_FORECAST'] = 'Šis spraudnis atspoguļo laika apstākļu prognozi no attiecīgās atrašanas vietas un attēla vai video datumu.';
+$lang['VIEW_FORECAST'] = 'Skatīt ziņas DarkSky.net';
+$lang['UNIT_DESC'] = 'Izvēlieties attiecīgās vienības. Pēc noklusējuma ir auto.';
+$lang['SHOW_MODE_DESC'] = 'Izvēlieties grafisko vai tekstuālo režīmu aplikācijai';
+$lang['HEADER_DESC'] = 'Ierakstiet nosaukumu, lai parādītu to virs laika apstākļu detaļām.';
+$lang['SHOW_LINK_DESC'] = 'Pievienot saiti pēc laika apstākļu maiņas, lai skatītu to DarkSky.net.';
+$lang['FORECAST_PLUGIN_BY'] = 'Spraudnis <a href="https://github.com/xbgmsharp/piwigo-forecast" target="_blank">xbgmsharp</a>';
+$lang['COLOR_TEXT_DESC'] = 'Teksta krāsa un animēta ikona, pēc noklusējuma ir melna.';
+$lang['SHOW_MODE'] = 'Rādīt grafiskā vai teksta režīmā';
+$lang['SHOW_LINK'] = 'Atainot \'View on Forecast\'';
+$lang['UNIT'] = 'Vienība';
+$lang['ADD_BEFORE'] = 'Pievienot laika ziņas vispirms';
+$lang['API_KEY_DESC'] = 'Izvēles, ievadiet savu DarkSky.net API atslēgu vai izmantojiet proxy starpniekserveri.';
+$lang['LANGUAGE'] = 'Valoda';
+$lang['HEADER'] = 'Galvenes nosaukums';
+$lang['COLOR_BKG_DESC'] = 'Fona krāsa, pēc noklusējuma ir balta.';
+$lang['COLOR_TEXT'] = 'Krāsu teksts';
+$lang['COLOR_BKG'] = 'Krāsu fons';
+$lang['API_KEY'] = 'API atslēga';
+$lang['ADD_BEFORE_DESC'] = 'Kur atainot laika ziņu informāciju';
